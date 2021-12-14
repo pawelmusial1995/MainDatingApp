d445b3571628b6819cdaf50d4064df6120810b02
Pawel <pawelmusial1995@gmail.com>
2021-12-14 21:34:20 +0100

    Section  7 improvements

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
@@ -15,13 +16,14 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     children: 
     [
+      {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
       {path: 'members/:id', component: MemberDetailComponent},
       {path: 'lists', component: ListsComponent},
       {path: 'messages', component: MessagesComponent},
     ]
   },
-  {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
  
+  {path: 'errors', component: TestErrorsComponent},
   {path: '**', component: HomeComponent, pathMatch: 'full'},
 
 ];
